b6e8cf045d6c39276a5ab6c6f3302ce895d77e42
Vyacheslav Frolov <algebris@gmail.com>
2018-04-28 04:13:06 +0700

    changed format of storage

--- routes/addrRouter.js
+++ routes/addrRouter.js
@@ -8,42 +8,25 @@ const db = require('../utils/db');
 const convertFromSatoshi = value => parseInt(value) / 100000000;
 
 router.get('/balance/:addr', async (req, res, next) => {
-  const data = await db.client.hmget(`addr:${req.params.addr}`, ['received', 'sent', 'balance']);
+  const data = await db.client.hmget(`addr:${req.params.addr}`, ['sent', 'received', 'staked', 'balance',]);
   if(data === null) {
     res.status(400).json({err: 'Unknown address'});
   } else {
     res.json({
-      received: convertFromSatoshi(data[0]), 
-      sent: convertFromSatoshi(data[1]),
-      balance: convertFromSatoshi(data[2])
+      sent: convertFromSatoshi(data[0]),
+      received: convertFromSatoshi(data[1]), 
+      staked: convertFromSatoshi(data[2]),
+      balance: convertFromSatoshi(data[3])
     });
   }
 });
 
 router.get('/listunspent/:addr', async (req, res, next) => {
-  let data = await db.client.hgetall(`addr.utxo:${req.params.addr}`);
+  let data = await db.client.lrange(`addr.utxo:${req.params.addr}`, 0, -1);
   
   if(data === null) {
     res.json({err: 'Unknown address'});
   } else {
-    data = _.chain(data)
-      .transform((acc, val, key) => {
-        const [tx_hash, tx_pos] = key.split(':');
-        try {
-          val = JSON.parse(val)
-        } catch(err) {
-          return next(err);
-        }
-        acc.push({ 
-          tx_hash, 
-          tx_pos: parseInt(tx_pos, 10),
-          value: val.amount,
-          height: val.height 
-        });
-      },[])
-      .sortBy('height')
-      .values();
-
     res.json(data);
   }
 });
